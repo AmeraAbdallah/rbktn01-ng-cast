b6e0ff438251ad102a69567dfe505a9f8d65bbc8
AmeraAbdallah <amera.ayad.it@gmail.com>
2019-12-12 18:45:46 +0100

    sprint 11 12 dec day 2

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,25 @@ angular.module('video-player')
 
 .component('app', {
   controller: function () {
-    this.selectVideo = (video) => {this.currentVideo = video};
-    this.searchResults = () => {console.log('ss')};
+    this.autoplay = 0;
+    this.autoFlag = false;
+    this.selectVideo = (video) => {
+      if (!!this.autoFlag) {
+        this.autoplay = 1;
+      }
+      this.currentVideo = video
+    };
+    this.searchResults = (result) => {
+      this.videos = result;
+      this.selectVideo(this.videos[0])
+      this.autoFlag = true;
+    };
     this.videos = window.exampleVideoData;
-    this.currentVideo = this.videos[0];
+    this.currentVideo = !!this.videos ? this.videos[0] : {};
     this.test = (video) => {
       console.log(video)
-    }
+    };
+    
   },
   templateUrl: 'src/templates/app.html'
 });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -3,10 +3,12 @@ angular.module('video-player')
 .component('videoPlayer', {
   templateUrl:'src/templates/videoPlayer.html',
   bindings: {
-    currentVideo: '='
+    video: '=',
+    autoplay: '<'
   }, 
-  controller: ($scope) => {
-    this.id = `https://www.youtube.com/embed/${$scope.$parent.$ctrl.currentVideo.id.videoId}`
-    console.log($scope.$parent.$ctrl.currentVideo)
+  controller: function() {
+    this.url = (id, auto = 0) => {
+      return `https://www.youtube.com/embed/${id}?autoplay=${auto}`
+    };
   },
 });
